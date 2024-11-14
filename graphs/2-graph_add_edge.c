42cb00b65afe386fbe588a8be1f2c3c8afa6e858
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-11-14 04:15:42 -0600

    alt

--- graphs/2-graph_add_edge.c
+++ graphs/2-graph_add_edge.c
@@ -1,5 +1,34 @@
 #include "graphs.h"
 
+int vrtx_add_edge(vertex_t *src, vertex_t *dest);
+
+/**
+ * vrtx_add_edge - adds an edge from a source vertex to a destination vertex
+ * @src: pointer to source vertex
+ * @dest: pointer to destination vertex
+ * Return: 1 on success, else 0
+ */
+int vrtx_add_edge(vertex_t *src, vertex_t *dest)
+{
+	edge_t *new_edge, *edge;
+
+	new_edge = malloc(sizeof(edge_t));
+	if (new_edge == NULL)
+		return (0);
+	new_edge->dest = dest;
+	new_edge->next = NULL;
+	if (src->edges == NULL)
+		src->edges = new_edge;
+	else
+	{
+		for (edge = src->edges; edge->next; edge = edge->next)
+			;
+		edge->next = new_edge;
+	}
+	src->nb_edges++;
+	return (1);
+}
+
 /**
  * graph_add_edge - adds an edge between two vertices to an existing graph
  * @graph: pointer to the graph to add the edge to
@@ -9,46 +38,22 @@
  * Return: 1 on success, else 0
  */
 int graph_add_edge(graph_t *graph, const char *src,
-				   const char *dest, edge_type_t type)
+		   const char *dest, edge_type_t type)
 {
 	vertex_t *src_vertex = NULL, *dest_vertex = NULL, *vertex;
-	edge_t *new_edge_src;
 
-	if (graph == NULL || src == NULL || dest == NULL)
-		return (0); /* vvv search for src and dest vertices vvv */
-	for (vertex = graph->vertices; vertex != NULL; vertex = vertex->next)
+	if (!graph || !src || !dest)
+		return (0);
+	for (vertex = graph->vertices; vertex; vertex = vertex->next)
 	{
 		if (strcmp(vertex->content, src) == 0)
 			src_vertex = vertex;
 		if (strcmp(vertex->content, dest) == 0)
 			dest_vertex = vertex;
-		if (src_vertex != NULL && dest_vertex != NULL)
-			break;
 	}
-	if (src_vertex == NULL || dest_vertex == NULL)
+	if (!src_vertex || !dest_vertex || !vrtx_add_edge(src_vertex, dest_vertex))
 		return (0);
-	new_edge_src = malloc(sizeof(edge_t)); /* cre8 edg frm src_vtx 2 dst_vtx */
-	if (new_edge_src == NULL)
-		return (0);
-	new_edge_src->dest = dest_vertex;
-	new_edge_src->next = src_vertex->edges;
-	src_vertex->edges = new_edge_src;
-	src_vertex->nb_edges++;
 	if (type == BIDIRECTIONAL)
-	{
-		edge_t *new_edg_dst = malloc(sizeof(edge_t));
-
-		if (new_edg_dst == NULL)
-		{
-			src_vertex->edges = new_edge_src->next; /* undo previous edge */
-			src_vertex->nb_edges--;
-			free(new_edge_src);
-			return (0);
-		}
-		new_edg_dst->dest = src_vertex;
-		new_edg_dst->next = dest_vertex->edges;
-		dest_vertex->edges = new_edg_dst;
-		dest_vertex->nb_edges++;
-	}
+		return (vrtx_add_edge(dest_vertex, src_vertex));
 	return (1);
 }
