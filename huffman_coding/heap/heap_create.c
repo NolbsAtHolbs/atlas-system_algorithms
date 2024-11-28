5213c4e4c53f4e190d1cd331649c69aee6e12a72
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-11-28 10:34:43 -0600

    made a heap

--- huffman_coding/heap/heap_create.c
+++ huffman_coding/heap/heap_create.c
@@ -1,1 +1,21 @@
+#include "heap.h"
 
+/**
+ * heap_create - creates a heap data structure
+ * @data_cmp: pointer to a function to compare two nodes' data
+ * Return: pointer to the created heap_t structure, or NULL on failure
+ */
+heap_t *heap_create(int (*data_cmp)(void *, void *))
+{
+	heap_t *heap;
+
+	if (data_cmp == NULL)
+		return (NULL);
+	heap = malloc(sizeof(heap_t));
+	if (heap == NULL)
+		return (NULL);
+	heap->size = 0;
+	heap->root = NULL;
+	heap->data_cmp = data_cmp;
+	return (heap);
+}
