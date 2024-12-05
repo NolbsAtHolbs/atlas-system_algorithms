e67a2c8d78c9d8bff8257bdec823b7809f2f6cf6
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-12-04 21:28:10 -0600

    made files

---
+++ pathfinding/pathfinding.h
@@ -0,0 +1,29 @@
+#ifndef PATHFINDING_H
+#define PATHFINDING_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "graphs.h"
+#include "queues.h"
+
+/**
+ * struct point_s - Structure storing coordinates
+ *
+ * @x: X coordinate
+ * @y: Y coordinate
+ */
+typedef struct point_s
+{
+	int x;
+	int y;
+} point_t;
+
+queue_t *backtracking_array(char **map, int rows, int cols,
+							point_t const *start, point_t const *target);
+queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
+							vertex_t const *target);
+queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
+						vertex_t const *target);
+
+#endif
