c2132183f1bc85b9b92e4ad8d1ab32fd9f6b29d6
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-12-04 20:44:56 -0600

    built a huffman tree

---
+++ huffman_coding/huffman_tree.c
@@ -0,0 +1,31 @@
+#include "huffman.h"
+
+/**
+ * huffman_tree - builds the Huffman tree
+ * @data: array of characters
+ * @freq: array of frequencies
+ * @size: size of the arrays
+ * Return: pointer to root node of Huffman tree, or NULL on failure
+ */
+binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
+{
+	binary_tree_node_t *root;
+	heap_t *priority_queue;
+
+	if (!data || !freq || size == 0)
+		return (NULL); /* invalid input */
+	priority_queue = huffman_priority_queue(data, freq, size); /* create PQ */
+	if (!priority_queue)
+		return (NULL); /* failed to create PQ */
+	while (priority_queue->size > 1) /* build the huffman tree */
+	{
+		if (!huffman_extract_and_insert(priority_queue))
+		{
+			heap_delete(priority_queue, NULL); /* prevents memory leaks */
+			return (NULL); /* failed during extraction and insertion */
+		}
+	}
+	root = heap_extract(priority_queue); /* xtract root node of huffman tree */
+	heap_delete(priority_queue, NULL);
+	return (root);
+}
