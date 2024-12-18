f5e5d8226736c397835e0db6ce2887364b1af81a
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2024-12-17 23:41:54 -0600

    diameter by edges of longest path

--- nary_trees/3-nary_tree_diameter.c
+++ nary_trees/3-nary_tree_diameter.c
@@ -6,12 +6,11 @@
  * @diameter: pointer to current maximum diameter found
  * Return: node height
  */
-static size_t nary_height_diameter(const nary_tree_t *root,
-						   size_t *diameter)
+static size_t nary_height_diameter(nary_tree_t const *root, size_t *diameter)
 {
-	size_t max_h1 = 0, max_h2 = 0;
+	size_t max_h1 = 0, max_h2 = 0; /* top two child heights */
 	size_t child_height;
-	const nary_tree_t *child;
+	nary_tree_t const *child;
 
 	if (!root)
 		return (0);
@@ -27,27 +26,27 @@ static size_t nary_height_diameter(const nary_tree_t *root,
 		else if (child_height > max_h2)
 			max_h2 = child_height;
 		child = child->next;
-	} /* update diameter if sum of two largest heights is bigger */
+	} /* update diameter if needed */
 	if (max_h1 + max_h2 > *diameter)
 		*diameter = max_h1 + max_h2;
 	if (!root->children)
-		return (0); /* no children, no height */
-	return (max_h1 + 1); /* else height = top child's height + 1 edge */
+		return (0); /* no child, no height */
+	return (max_h1 + 1); /* otherwise, height = tallest child's height + 1 */
 }
 
 /**
- * nary_tree_diameter - computes the diameter of an N-ary tree
- * @root: pointer to root of the tree
- * Returns: 0 if tree is empty, diameter if not empty (diameter in edges)
+ * nary_tree_diameter - computes diameter of an N-ary tree
+ * @root: pointer to root node
+ * Returns the diameter of the tree, or 0 if empty.
  */
-size_t nary_tree_diameter(const nary_tree_t *root)
+size_t nary_tree_diameter(nary_tree_t const *root)
 {
 	size_t diameter = 0;
 
 	if (!root)
-		return 0;
+		return (0);
 	nary_height_diameter(root, &diameter);
 	if (diameter == 0)
 		return (0);
 	return (diameter + 1);
-} /* if root exists, add 1 to diameter to count edges properly */
+} /* if there's at least one node, return diameter + 1 */
